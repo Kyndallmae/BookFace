8b46f7f14a0d0adf4a14a1ef2199e611f0e5ea93
Kyndallmae <kyndallmae@gmail.com>
2023-11-21 17:10:36 -0600

    modified thought.js models

--- Assets/Develope/models/Thoughts.js
+++ Assets/Develope/models/Thoughts.js
@@ -1,45 +1,70 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require("mongoose");
 
-// Schema to create User model
-const thoughtSchema = new Schema(
+const reactionSchema = new Schema(
     {
-        first: String,
-        last: String,
-        age: Number,
-        posts: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: 'post',
-            },
-        ],
+      reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => {
+          new Types.ObjectId()
+        },
+      },
+      reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280,
+      },
+      username: {
+        type: String,
+        required: true,
+      },
+      createdAt: {
+        type: Date,
+        default: () => {
+          new Date().toLocaleString()
+        },
+      }
     },
     {
-        // Mongoose supports two Schema options to transform Objects after querying MongoDb: toJSON and toObject.
-        // Here we are indicating that we want virtuals to be included with our response, overriding the default behavior
-        toJSON: {
-            virtuals: true,
-        },
-        id: false,
+      toJSON: {
+        getters: true,
+      },
+      id: false,
     }
 );
-const reactionSchema = new mongoose.Schema({
-    username: { type: String, required: true },
-    // This will add a single subdocument to include the manager's information
-    reactionId: [reactionIdSchema],
-    // This will include an array that holds all the employees' information
-    reactionBody: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now },
-  });
 
-// Create a virtual property `fullName` that gets and sets the user's full name
-userSchema
-    .virtual('reactionCount')
-    // Getter
-    .get(function () {
-        return this.reactions.length;
-    });
 
-// Initialize our User model
-const Thought = model('thought', thoughtSchema);
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: () => {
+        new Date().toLocaleString()
+      },
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  },
+);
 
-module.exports = User;
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model("thought", thoughtSchema);
+
+module.exports = Thought;
