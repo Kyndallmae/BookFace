7e6abe7baa407e4db51c84a78a528821a4b3ba9d
Kyndallmae <kyndallmae@gmail.com>
2023-11-21 17:41:02 -0600

    modified connection

--- Assets/Develope/config/connection.js
+++ Assets/Develope/config/connection.js
@@ -1,7 +1,7 @@
 const { connect, connection } = require('mongoose');
 
 const connectionString =
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialNetworkDB';
+  process.env.MONGODB_URI || 'mongodb+srv://Carothers:Corrine113090!@cluster0.7eyvk8w.mongodb.net/?retryWrites=true&w=majority';
 
 connect(connectionString);
 
